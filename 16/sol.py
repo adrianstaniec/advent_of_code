8f7f76c58e48788c6556795bf04a97d429804ae9
Adrian Staniec <adrianstaniec@gmail.com>
2020-12-16 14:56:03 +0100

    add day 16 part 2 solution

--- 16/sol.py
+++ 16/sol.py
@@ -1,9 +1,11 @@
-"""My solution to https://adventofcode.com/2020/day/16"""
+"""My s lution to https://adventofcode.com/2020/day/16"""
 
 import argparse
 import fileinput
 import logging
 import sys
+import operator
+from math import prod
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--debug", action="store_true")
@@ -50,6 +52,13 @@ def parse():
     return rules, my_ticket, nearby_tickets
 
 
+def value_according_to_rule(value, rule):
+    range1, range2 = rule
+    return (range1[0] <= value and value <= range1[1]) or (
+        range2[0] <= value and value <= range2[1]
+    )
+
+
 def valid_with_any_rule(value, rules):
     valid = False
     for range1, range2 in rules.values():
@@ -57,13 +66,20 @@ def valid_with_any_rule(value, rules):
             valid = True
         if range2[0] <= value and value <= range2[1]:
             valid = True
-    logger.debug(f"value {value} is valid: {valid}")
     return valid
+
+
+def every_field_valid_with_some_rule(ticket, rules):
+    for value in ticket:
+        if not valid_with_any_rule(value, rules):
+            return False
+    return True
 
 
 if __name__ == "__main__":
     rules, my_ticket, nearby_tickets = parse()
 
+    logger.info("--- Part One ---")
     invalid_values = [
         value
         for ticket in nearby_tickets
@@ -71,4 +87,59 @@ if __name__ == "__main__":
         if not valid_with_any_rule(value, rules)
     ]
     logger.debug(invalid_values)
-    print(sum(invalid_values))
+    logger.info(sum(invalid_values))
+
+    logger.info("--- Part Two ---")
+
+    # discard tickets with invalid values
+    logger.debug(len(nearby_tickets))
+    nearby_tickets = [
+        ticket
+        for ticket in nearby_tickets
+        if every_field_valid_with_some_rule(ticket, rules)
+    ]
+    logger.debug(len(nearby_tickets))
+
+    # NOTE: I guess the assumption is that all the remaining nearby tickets are valid
+    # but it is certainly not guaranteed by the filtering above
+
+    # start with all possibilities
+    n_fields = len(rules)
+    col_to_field = {i: set(rules.keys()) for i in range(n_fields)}
+
+    # scan all tickets column by column
+    # to rule out classes that don't fit the data
+    for col in range(n_fields):
+        for ticket in nearby_tickets:
+            col_to_field[col] = {
+                field
+                for field in col_to_field[col]
+                if value_according_to_rule(ticket[col], rules[field])
+            }
+
+    logger.debug(f"\n{col_to_field=}")
+
+    # if thanks to elimination there is a single canidate per column:
+    # store it in figured dict
+    # and eliminate that candidate from the set of candidated for all other columns
+    # repeat until all columns are figured out
+    # NOTE: data is set up in a way that it will work
+    figured = {}
+    while len(col_to_field) > 0:
+        i = min(col_to_field, key=lambda k: len(col_to_field[k]))
+        if len(col_to_field[i]) > 1:
+            break
+        figured[i] = col_to_field[i]
+        del col_to_field[i]
+        for col in col_to_field:
+            col_to_field[col] -= figured[i]
+
+    logger.debug(f"\n{figured=}")
+
+    field_no = {v.pop(): k for k, v in figured.items()}
+    logger.debug([my_ticket[v] for k, v in field_no.items() if k.startswith("departure")])
+    answer = prod(my_ticket[v] for k, v in field_no.items() if k.startswith("departure"))
+
+    logger.debug(f"\n{field_no=}")
+    logger.debug(f"\n{my_ticket=}")
+    logger.info(answer)
